e54762d60cef676fd196a66858591878e4cad79f
sangmin99554 <sangmin99554@naver.com>
2019-09-26 02:10:56 +0900

    added lines to switch image of widget but didnt work. working on

--- app/src/main/java/com/example/schedule/ExampleAppWidgetProvider.java
+++ app/src/main/java/com/example/schedule/ExampleAppWidgetProvider.java
@@ -4,19 +4,40 @@ import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.ImageView;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 public class ExampleAppWidgetProvider extends AppWidgetProvider {
 
+    static Context context;
+    static Uri uri;
+    public ExampleAppWidgetProvider(Context c) {
+        context = c;
+
+    }
+    ImageView widgetView;
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         for (int appWidgetId : appWidgetIds) {
+//            Toast.makeText(context, "onUpdate():" , Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.example_widget);
-//            views.setImageViewResource(R.id.scrImage, R.drawable.screenshot);
+//            views.setImageViewUri(R.id.scrImage, uri);
+            views.setOnClickPendingIntent(R.id.scrImage, pendingIntent);
             appWidgetManager.updateAppWidget(appWidgetId, views);
 
-            System.out.println("updated");
+
         }
+    }
+
+    public void aaa(Uri image) {
+        uri = image;
+//        widgetView = (ImageView)findViewById(R.id.scrImage);
     }
 }
 
