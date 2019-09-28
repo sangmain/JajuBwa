3e3ad7cc40d77fa09f51bbecf1c685d734fa0397
sangmin99554 <sangmin99554@naver.com>
2019-09-29 03:57:25 +0900

    FEAT: Add get image user selected from activity_main.xml but not complete

--- app/src/main/java/com/example/schedule/ExampleAppWidgetProvider.java
+++ app/src/main/java/com/example/schedule/ExampleAppWidgetProvider.java
@@ -5,39 +5,46 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.widget.ImageView;
+import android.util.Log;
 import android.widget.RemoteViews;
 import android.widget.Toast;
 
+
 public class ExampleAppWidgetProvider extends AppWidgetProvider {
 
-    static Context context;
     static Uri uri;
-    public ExampleAppWidgetProvider(Context c) {
-        context = c;
+    Bitmap bitmap;
 
-    }
-    ImageView widgetView;
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        Log.d("debug", "update function called");
+
+        bitmap = BitmapFactory.decodeResource(context.getResources(),R.drawable.screenshot);
+
+        if (MainActivity.bitmap != null) {
+            Log.d("debug", "bitmap is not null");
+            bitmap = MainActivity.bitmap;
+        }
         for (int appWidgetId : appWidgetIds) {
-//            Toast.makeText(context, "onUpdate():" , Toast.LENGTH_SHORT).show();
+            Toast.makeText(context, "onUpdate():", Toast.LENGTH_LONG).show();
+            Log.d("debug", "update in widget");
             Intent intent = new Intent(context, MainActivity.class);
             PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
 
+
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.example_widget);
-//            views.setImageViewUri(R.id.scrImage, uri);
             views.setOnClickPendingIntent(R.id.scrImage, pendingIntent);
+            views.setImageViewBitmap(R.id.scrImage, bitmap);
+//            ComponentName thisWidget = new ComponentName(context, ExampleAppWidgetProvider.class);
+//            appWidgetManager.updateAppWidget(thisWidget, views);
+
+
             appWidgetManager.updateAppWidget(appWidgetId, views);
 
-
         }
-    }
-
-    public void aaa(Uri image) {
-        uri = image;
-//        widgetView = (ImageView)findViewById(R.id.scrImage);
     }
 }
 
