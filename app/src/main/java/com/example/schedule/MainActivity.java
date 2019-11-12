1abb7d7137375b6b4199d338899413d2ee0bd4a5
sangmin99554 <sangmin99554@naver.com>
2019-11-12 23:48:18 +0900

    Update MainActivity.java
    Remove features and add to ImageWidgetConfig

--- app/src/main/java/com/example/schedule/MainActivity.java
+++ app/src/main/java/com/example/schedule/MainActivity.java
@@ -1,80 +1,42 @@
 package com.example.schedule;
 
-import android.appwidget.AppWidgetManager;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.util.Log;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.RemoteViews;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.github.chrisbanes.photoview.PhotoView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView imageView;
-    Uri imageUri;
-    public static Bitmap bitmap;
-    private static final int PICK_IMAGE = 100;
 
+//
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        imageView = findViewById(R.id.current_image);
-        bitmap = ((BitmapDrawable)imageView.getDrawable()).getBitmap();
-        if (bitmap != null)
-            imageView.setImageBitmap(bitmap);
     }
 
-    public void confirmConfiguration(View v)
-    {
-        openGallery();
-    }
+//    public void setImageView()
+//    {
+////        String myurl = "http://sangmin99554.pythonanywhere.com/media/screenshot.PNG";
+//       String myurl = "http://sangmin99554.pythonanywhere.com/media/1569776319546.png";
+//
+//        Picasso.get().load(myurl).placeholder(R.drawable.loading).into(imageView);
+//
+//    }
 
-    public void onclick_zoom(View view){
-        AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
-        View mView = getLayoutInflater().inflate(R.layout.dialog_custom_layout, null);
-        PhotoView photoView = mView.findViewById(R.id.imageView);
-        photoView.setImageBitmap(bitmap);
-        mBuilder.setView(mView);
-        AlertDialog mDialog = mBuilder.create();
-        mDialog.show();
-    }
-    private void openGallery() {
-        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
-        startActivityForResult(gallery, PICK_IMAGE);
-    }
+//
+//    public void onclick_zoom(View view){
+//        AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
+//        View mView = getLayoutInflater().inflate(R.layout.dialog_custom_layout, null);
+//        PhotoView photoView = mView.findViewById(R.id.imageView);
+//        if (bitmap == null)
+//            photoView.setImageResource(R.drawable.screenshot);
+//        else
+//            photoView.setImageBitmap(bitmap);
+//        mBuilder.setView(mView);
+//        AlertDialog mDialog = mBuilder.create();
+//        mDialog.show();
+//    }
 
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode == RESULT_OK &&  requestCode == PICK_IMAGE) {
-            imageUri = data.getData();
-            imageView.setImageURI(imageUri);
-            bitmap = ((BitmapDrawable)imageView.getDrawable()).getBitmap();
-
-            Log.d(" act main", "image selected is updated");
-
-            Context context = this;
-            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
-            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.example_widget);
-            ComponentName thisWidget = new ComponentName(context, ExampleAppWidgetProvider.class);
-            remoteViews.setImageViewBitmap(R.id.scrImage, bitmap);
-
-            appWidgetManager.updateAppWidget(thisWidget, remoteViews);
-
-        }
-    }
+//
 }
