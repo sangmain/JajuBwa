17821b0af0576ddf78af742d4c257673d7a452a6
sangmin99554 <sangmin99554@naver.com>
2019-11-12 23:49:19 +0900

    Add ImageAppWidgetConfig.java
    Add features of MainActivity.java and simplify

---
+++ app/src/main/java/com/example/schedule/ImageAppWidgetConfig.java
@@ -0,0 +1,114 @@
+package com.example.schedule;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RemoteViews;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.ByteArrayOutputStream;
+
+public class ImageAppWidgetConfig extends AppCompatActivity {
+
+    public static final String SHARED_PREFS = "prefs";
+    public static final String KEY_BUTTON_TEXT = "keyButtonText";
+
+    private int appWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
+    private ImageView imageView;
+
+
+    Uri imageUri;
+    public static Bitmap bitmap;
+    private static final int PICK_IMAGE = 100;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_app_widget_config);
+
+        Intent configIntent = getIntent();
+        Bundle extras = configIntent.getExtras();
+        if (extras != null) {
+            appWidgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID,
+                    AppWidgetManager.INVALID_APPWIDGET_ID);
+        }
+
+        Intent resultValue = new Intent();
+        resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+        setResult(RESULT_CANCELED, resultValue);
+
+        if (appWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID) {
+            finish();
+        }
+
+        imageView = findViewById(R.id.view_preview);
+    }
+
+    public void select_from_file(View v)
+    {
+        openGallery(v);
+    }
+
+    private void openGallery(View v) {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        if(resultCode == RESULT_OK &&  requestCode == PICK_IMAGE) {
+            imageUri = data.getData();
+            imageView.setImageURI(imageUri);
+            bitmap = ((BitmapDrawable)imageView.getDrawable()).getBitmap();
+//
+//            Log.d(" act main", "image selected is updated");
+//
+//            Context context = this;
+
+            RemoteViews remoteViews = new RemoteViews(this.getPackageName(), R.layout.image_widget);
+            ComponentName thisWidget = new ComponentName(this, ImageAppWidgetProvider.class);
+            remoteViews.setImageViewBitmap(R.id.view_image_widget, bitmap);
+
+            appWidgetManager.updateAppWidget(thisWidget, remoteViews);
+
+            String str_image = BitMapToString(bitmap);
+            SharedPreferences prefs = getSharedPreferences("str_image", MODE_PRIVATE);
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putString("imagestrings", str_image);
+            editor.apply();
+
+            Intent resultValue = new Intent();
+            resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+            setResult(RESULT_OK, resultValue);
+            finish();
+        }
+    }
+
+    public String BitMapToString(Bitmap bitmap){
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+        byte [] b = baos.toByteArray();
+        String temp = Base64.encodeToString(b, Base64.DEFAULT);
+        return temp;
+    }
+}
